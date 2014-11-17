86e7eb12bfaca94ef866c4ce97fdddfd287a55be
stephlascola <stephlascola347@gmail.com>
2014-11-17 11:16:08 -0800

    Move Game over title down

--- game/GameOverTitle.java
+++ game/GameOverTitle.java
@@ -14,6 +14,10 @@ public class GameOverTitle extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+       moveDown(); // Add your action code here.
     }    
+    public void moveDown()
+    {
+        setLocation (getX(), getY() + 3);
+    }
 }
