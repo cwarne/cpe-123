e806df868468df7c2f767c73b0ef1d251904821b
michaeltornatta <musicalmichael11@mail.com>
2014-12-02 19:03:51 -0800

    Fixed Currency Glitch (again)

--- game/CurrencyCounter.java
+++ game/CurrencyCounter.java
@@ -6,7 +6,7 @@ import java.awt.Color;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class CurrencyCounter extends Currency
+public class CurrencyCounter extends CurrencyTracking
 {
     /** Added ~ Michael Tornatta **/
     private GreenfootImage board;
--- game/CurrencyTracking.java
+++ game/CurrencyTracking.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class CurrencyIndicator here.
+ * Write a description of class CurrencyTracking here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class CurrencyIndicator extends CurrencyTracking
+public class CurrencyTracking extends Actor
 {
     /**
-     * Act - do whatever the CurrencyIndicator wants to do. This method is called whenever
+     * Act - do whatever the CurrencyTracking wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
