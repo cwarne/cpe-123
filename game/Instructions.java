209b71afa26d6b61a57cb436972f3565385feb1a
nickjon9933 <nickjones2@comcast.net>
2014-12-02 20:55:29 -0800

    intro screen
    small delay so intro screen is not skipped when space is pressed

--- game/Instructions.java
+++ game/Instructions.java
@@ -1,18 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Instructions here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+  /**
+  * Write a description of class Instructions here.
+  * 
+  * @author (your name) 
+  * @version (a version number or a date)
+   */
+  
 public class Instructions extends World
 {
-
     /**
      * Constructor for objects of class Instructions.
-     * 
+     * timer for instructions
+     * @Nick Jones
      */
+     public int coolDownTimer = 1;
+     public int coolDownLimit = 40;
     public Instructions()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -25,14 +28,25 @@ public class Instructions extends World
     /**
      * Change screen to Game mode
      * @StephanieLascola
+     * @NickJones
      */
     public void enterGameWorld()
     {
-        if((Greenfoot.mouseClicked(this)) || (Greenfoot.isKeyDown("space")))
+        if(coolDownTimer == 0)
+        {
+        if((Greenfoot.mouseClicked(this) || Greenfoot.isKeyDown("space")))
         {
             GameWorld gameworld = new GameWorld();
             Greenfoot.setWorld(gameworld);
         }
     }
-
+     if((coolDownTimer < coolDownLimit) && (coolDownTimer >0))
+        {
+            coolDownTimer++;
+        }
+    if( coolDownLimit <= coolDownTimer)
+    {
+        coolDownTimer = 0;
+    }
+    }
 }
